7f32ac957f10d7ee861c54afacb0fc9c76ff2200
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-04-19 00:07:29 -0600

    client cleanup / target / peds
    - Cleanup function for resource stop. Remove peds, blips and prompts
    - Spawn bank peds using vector4
    - Target is TargetEntity when target convar = true
    - Prompts are set by Y offset from ped spawn
    - Remove unused code

--- client/client.lua
+++ client/client.lua
@@ -1,18 +1,59 @@
 local QRCore = exports['qr-core']:GetCoreObject()
 local banks
 local showing, playerLoaded = false, false
+local BankPed = {}
+local BankBlips = {}
 InBank = false
-blips = {}
 
-
-RegisterNetEvent('qr-banking:client:syncBanks')
-AddEventHandler('qr-banking:client:syncBanks', function(data)
-    banks = data
-    if showing then
-        showing = false
+-- Remove Peds / Blips / Prompts --
+local function BankCleanup()
+    for banks, v in pairs(Config.BankLocations) do
+        if not Config.UseTarget then exports['qr-core']:deletePrompt(v.name) end
+        if BankPed[banks] ~= nil then
+            DeletePed(BankPed[banks])
+            BankPed[banks] = nil
+        end
+        if BankBlips[banks] ~= nil then
+            RemoveBlip(BankBlips[banks])
+            BankBlips[banks] = nil
+        end
     end
-end)
+end
 
+local function _GET_DEFAULT_RELATIONSHIP_GROUP_HASH ( iParam0 )
+    return Citizen.InvokeNative( 0x3CC4A718C258BDD0 , iParam0 );
+end
+
+local function SET_PED_RELATIONSHIP_GROUP_HASH ( iVar0, iParam0 )
+    return Citizen.InvokeNative( 0xC80A74AC829DDD92, iVar0, _GET_DEFAULT_RELATIONSHIP_GROUP_HASH( iParam0 ) )
+end
+
+local function modelRequest(model)
+    CreateThread(function() RequestModel(model) end)
+end
+
+-- Spawn Ped --
+local function QRSpawnPed(model, coords)
+    local pedmodel = GetHashKey(model)
+
+    while not HasModelLoaded(GetHashKey(model)) do Wait(500) modelRequest(GetHashKey(model)) end
+
+    local pedId = CreatePed(pedmodel, coords.x, coords.y, coords.z - 1, coords.w, false, false, 0, 0)
+    while not DoesEntityExist(pedId) do Wait(300) end
+
+    Citizen.InvokeNative(0x283978A15512B2FE, pedId, true)
+    FreezeEntityPosition(pedId, true)
+    SetEntityInvincible(pedId, true)
+    TaskStandStill(pedId, -1)
+    SetBlockingOfNonTemporaryEvents(pedId, true)
+    SET_PED_RELATIONSHIP_GROUP_HASH(pedId, pedmodel)
+    SetEntityCanBeDamagedByRelationshipGroup(pedId, false, `PLAYER`)
+    SetEntityAsMissionEntity(pedId, true, true)
+    SetModelAsNoLongerNeeded(pedmodel)
+    return pedId
+end
+
+-- Open Bank UI --
 function openAccountScreen()
     QRCore.Functions.TriggerCallback('qr-banking:getBankingInformation', function(banking)
         if banking ~= nil then
@@ -28,62 +69,56 @@ function openAccountScreen()
     end)
 end
 
-function atmRefresh()
-    QRCore.Functions.TriggerCallback('qr-banking:getBankingInformation', function(infor)
-        InBank = true
-        SetNuiFocus(true, true)
-        SendNUIMessage({
-            status = "refreshatm",
-            information = infor
-        })
-    end)
-end
-
-RegisterNetEvent('qr-banking:openBankScreen')
-AddEventHandler('qr-banking:openBankScreen', function()
-    openAccountScreen()
-end)
-
-Citizen.CreateThread(function()
+-- Threads / Startup --
+CreateThread(function()
     for banks, v in pairs(Config.BankLocations) do
-        if Config.UseTarget == false then
-            exports['qr-core']:createPrompt(v.name, v.coords, 0xF3830D8E, 'Open ' .. v.name, {
+        if BankPed[banks] == nil then BankPed[banks] = QRSpawnPed('A_M_M_BiVFancyTravellers_01', v.coords) end
+        if not Config.UseTarget then
+            local PrompCoords = GetOffsetFromEntityInWorldCoords(BankPed[banks], 0.0, 3.0, 0.0) -- Prompt 3 Units in Front of Ped
+            exports['qr-core']:createPrompt(v.name, vector3(PrompCoords.x, PrompCoords.y, PrompCoords.z), 0xF3830D8E, 'Open ' .. v.name, {
                 type = 'client',
                 event = 'qr-banking:openBankScreen',
                 args = { false, true, false },
             })
         else
-            exports['qr-target']:AddCircleZone(v.name, v.coords, 1, {
-                name = v.name,
-                debugPoly = false,
-              }, {
+            exports['qr-target']:AddTargetEntity(BankPed[banks], {
                 options = {
-                  {
-                    type = "client",
-                    event = 'qr-banking:openBankScreen',
-                    icon = "fas fa-dollar-sign",
-                    label = "Open Bank",
-                  },
+                    {
+                        type = "client",
+                        event = 'qr-banking:openBankScreen',
+                        icon = "fas fa-dollar-sign",
+                        label = "Open Bank",
+                      },
                 },
-                distance = 2.0,
-              })
+                distance = 3.0,
+            })
         end
         if v.showblip == true then
-            local StoreBlip = N_0x554d9d53f696d002(1664425300, v.coords)
-            SetBlipSprite(StoreBlip, -2128054417, 52)
-            SetBlipScale(StoreBlip, 0.2)
+            BankBlips[banks] = N_0x554d9d53f696d002(1664425300, v.coords)
+            SetBlipSprite(BankBlips[banks], -2128054417, 52)
+            SetBlipScale(BankBlips[banks], 0.2)
         end
     end
 end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     for k,v in pairs(Config.BankDoors) do
-        --for v, door in pairs(k) do
-        Citizen.InvokeNative(0xD99229FE93B46286,v,1,1,0,0,0,0)
-        Citizen.InvokeNative(0x6BAB9442830C7F53,v,0)
+        Citizen.InvokeNative(0xD99229FE93B46286, v, 1, 1, 0, 0, 0, 0)
+        Citizen.InvokeNative(0x6BAB9442830C7F53, v, 0)
     end
 end)
 
+-- Events --
+RegisterNetEvent('qr-banking:openBankScreen')
+AddEventHandler('qr-banking:openBankScreen', function()
+    openAccountScreen()
+end)
+
+RegisterNetEvent('qr-banking:client:syncBanks')
+AddEventHandler('qr-banking:client:syncBanks', function(data)
+    banks = data
+    if showing then showing = false end
+end)
 
 RegisterNetEvent('qr-banking:transferError')
 AddEventHandler('qr-banking:transferError', function(msg)
@@ -100,3 +135,6 @@ AddEventHandler('qr-banking:successAlert', function(msg)
         message = msg
     })
 end)
+
+-- Resource Stop / Cleanup --
+AddEventHandler('onResourceStop', function(resource) if resource ~= GetCurrentResourceName() then return end BankCleanup() end)
--- config.lua
+++ config.lua
@@ -1,19 +1,11 @@
 Config = {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qr-target interactions (don't change this, go to your server.cfg and add setr UseTarget true)
-Config.cardTypes = { "visa", "mastercard"}
-
-Config.ATMModels = {
-    -- "prop_atm_01",
-    -- "prop_atm_02",
-    -- "prop_atm_03",
-    -- "prop_fleeca_atm"
-}
 
 Config.BankLocations = {
-    {name = 'Bank of Rhodes',           bankType = "BANKS",     coords = vector3(1292.307, -1301.539, 77.04012),    showblip = true},
-    {name = 'Lemoyne National Bank',    bankType = "BANKS2",    coords = vector3(2644.579, -1292.313, 52.24956),    showblip = true},
-    {name = 'Valentine Savings Bank',   bankType = "BANKS3",    coords = vector3(-308.4189, 775.8842, 118.7017),    showblip = true},
-    {name = 'First National Bank',      bankType = "BANKS4",    coords = vector3(-813.1633, -1277.486, 43.63771),   showblip = true},
+    { name = 'Bank of Rhodes',           coords = vector4(1291.15, -1303.22, 77.04, 318.72),     	showblip = true },
+    { name = 'Lemoyne National Bank',    coords = vector4(2644.86, -1293.99, 52.25, 20.73),    		showblip = true },
+    { name = 'Valentine Savings Bank',   coords = vector4(-308.16, 773.93, 118.7, 4.16),    		showblip = true },
+    { name = 'First National Bank',      coords = vector4(-813.23, -1275.39, 43.64, 180.86),   		showblip = true },
 }
 
 Config.BankDoors = { --Doors that will always be open unless robbery has started
@@ -25,7 +17,7 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	334467483, -- inner door1
 	3718620420, -- inner door2
 	576950805, -- vault door
-	
+
 	-- Bank of Rhodes
 	3317756151, -- main door
 	3088209306, -- main door
@@ -33,7 +25,7 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	3142122679, -- inner door2
 	1634148892, -- inner door3
 	3483244267, -- vault
-  
+
 	-- Lemoyne National Bank Saint Denis
 	2158285782, -- main door
 	1733501235, -- main door
@@ -43,13 +35,13 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	965922748, -- manager door
 	1634115439, -- manager door
 	1751238140, -- vault
-	
+
 	-- West Elizabeth Co-Operative Bank Blackwater
 	531022111, -- main door
 	2117902999, -- inner door
 	2817192481, -- manager door
 	1462330364, -- vault door
-	
+
 	-- Bank of Armadillo
 	3101287960, -- main door
 	3550475905, -- inner door
